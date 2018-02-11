8ad1cde342050c58f2c489a91ad4c84e3f3108b7
Demis Bellot <demis.bellot@gmail.com>
2018-02-10 20:25:19 -0500

    Tech/Stack History tables needs View/Fav count columns as well

--- src/TechStacks.Tests/MigrationTasks.cs
+++ src/TechStacks.Tests/MigrationTasks.cs
@@ -1,25 +1,31 @@
+using NUnit.Framework;
 using ServiceStack.OrmLite;
 using TechStacks.ServiceModel.Types;
 using ServiceStack.Logging;
 
 namespace TechStacks.Tests
 {
-    [NUnit.Framework.Ignore("One-off Migration Tasks")]
+    [Ignore("One-off Migration Tasks")]
     public class MigrationTasks : DbTasksBase
     {
         static MigrationTasks() => LogManager.LogFactory = new ConsoleLogFactory(debugEnabled: true);
 
-        //[Test]
+        [Test]
         public void Add_ViewCounts()
         {
             using (var db = dbFactory.Open())
             {
-                //db.Select(db.From<TechnologyChoice>().Take(3)).PrintDump();
-
                 db.AddColumn<Technology>(x => x.ViewCount);
                 db.AddColumn<Technology>(x => x.FavCount);
+
                 db.AddColumn<TechnologyStack>(x => x.ViewCount);
                 db.AddColumn<TechnologyStack>(x => x.FavCount);
+
+                db.AddColumn<TechnologyStackHistory>(x => x.ViewCount);
+                db.AddColumn<TechnologyStackHistory>(x => x.FavCount);
+
+                db.AddColumn<TechnologyHistory>(x => x.ViewCount);
+                db.AddColumn<TechnologyHistory>(x => x.FavCount);
             }
         }
     }
