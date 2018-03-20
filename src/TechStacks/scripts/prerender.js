6c613b9586a74265c331c85de129deef715caff4
Demis Bellot <demis.bellot@gmail.com>
2018-03-20 02:18:10 -0400

    Change prerender cache to use realtime puppeteer node service

--- src/TechStacks/scripts/prerender.js
+++ src/TechStacks/scripts/prerender.js
@@ -1,30 +1,91 @@
-const puppeteer = require("puppeteer");
+const http = require('http');
+const puppeteer = require('puppeteer');
+const fetch = require('node-fetch');
+const delay = require('delay');
 
-const { JsonServiceClient } = require("@servicestack/client");
+const ProxyUrl = 'http://localhost:16325';
+const elementId = '__nuxt';
 
-const { StorePreRender } = require("./dtos");
+const port = 7000;
 
-var BaseUrl = 'http://localhost:16325';
+const IgnoreExtensions = ['svg','png','jpg','jpeg','gif','ico'];
+const TimeoutMs = 10000;
 
-var client = new JsonServiceClient(BaseUrl);
-client.headers.set("Content-Type", "text/html");
+(async () => {
+    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
 
-puppeteer.launch().then(async browser => {
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1366, height: 768 });
-    await page.goto(BaseUrl, {waitUntil: 'networkidle2'});
-    const html = await page.$eval('#__nuxt', e => e.innerHTML);
-
-    if (html) {
+    const requestHandler = async (req,res) => {
+        let page = null;
         try {
-            const request = new StorePreRender();
-            request.path = "/";
-            await client.putBody(request, html);
+
+            const info = req.url + " |ip| " + req.connection.remoteAddress + " |ua| " + req.headers['user-agent'];
+
+            if (IgnoreExtensions.some(x => req.url.endsWith(x))) {
+                console.log('ignoring: ' + info);
+                res.writeHeader(401, 'Ignored Extension');
+                res.end();
+                return;
+            }
+
+            const url = ProxyUrl + req.url;
+            console.log('fetch: ' + info);
+
+            page = await browser.newPage();
+            await page.setUserAgent('puppeteer');
+            await page.setViewport({ width: 1366, height: 768 });
+            await page.goto(url, {waitUntil: 'networkidle2'});
+
+            let html = null;
+            const start = new Date();
+
+            do {
+                try {
+                    html = elementId
+                        ? await page.$eval('#' + elementId, e => e.innerHTML)
+                        : await page.content();
+
+                    if (html)
+                        break;
+                } catch(e) {
+                    console.log(e);
+                }
+
+                var elapsed = new Date() - start;
+                if (elapsed > TimeoutMs) {
+                    console.log('timeout: ' + info)
+                    res.writeHeader(500, 'Timeout trying to access page content');
+                    res.end();
+                    return;
+                }
+
+                await delay(250);
+            } while (true);
+
+
+            res.writeHeader(200, {"Content-Type": "text/html"});  
+            res.write(html);  
+            res.end();             
         } catch(e) {
             console.log(e);
+        } finally {
+            if (page) { 
+                await page.close();
+            }
         }
-    }
+    };
 
-    // other actions...
-    await browser.close();
-});
+    const server = http.createServer(requestHandler)
+        
+    server.listen(port, (err) => {
+        if (err) {
+            return console.log('something bad happened', err)
+        }
+    
+        console.log(`server is listening on ${port}`)
+    });
+
+    process.on('exit', async () => {
+        await browser.close();
+    });
+
+})();
