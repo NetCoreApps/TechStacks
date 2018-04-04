b7f586611d70d83bec2abe86f96c9cf9dd5ee6b9
Demis Bellot <demis.bellot@gmail.com>
2018-04-03 22:03:10 -0400

    Create NotificationService + post-new/post-report/comment-report emails

---
+++ src/TechStacks.ServiceInterface/Admin/NotificationServices.Utils.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ServiceStack;
+using ServiceStack.OrmLite;
+using ServiceStack.Templates;
+using TechStacks.ServiceInterface.DataModel;
+using TechStacks.ServiceInterface.Notifications;
+using TechStacks.ServiceModel.Types;
+
+namespace TechStacks.ServiceInterface.Admin
+{
+    public class UserEmailInfo
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string DisplayName { get; set; }
+    }
+    
+    public partial class NotificationServices : Service
+    {
+        private Notification AssertNotification(long Id)
+        {
+            if (Id <= 0)
+                throw new ArgumentNullException(nameof(Id));
+
+            var notification = Db.SingleById<Notification>(Id);
+            if (notification == null)
+                throw HttpError.NotFound($"Notification not found {Id}");
+
+            return notification;
+        }
+
+        private async Task<Post> AssertPost(long postId)
+        {
+            var post = await Db.SingleByIdAsync<Post>(postId);
+            if (post == null)
+                throw HttpError.NotFound("Post not found: " + postId);
+
+            return post;
+        }
+
+        private TemplateContext CreateEmailTemplateContext() => new TemplateContext {
+                VirtualFiles = base.VirtualFiles,
+            }.Init();
+
+        private Task<List<int>> GetOrganizationModeratorIds(int organizationId)
+        {
+            return Db.ColumnAsync<int>(Db.From<OrganizationMember>()
+                .Where(x => x.OrganizationId == organizationId && (x.IsOwner || x.IsModerator))
+                .Select(x => x.UserId));
+        }
+
+        private async Task<EmailTemplate> CreateAndSaveEmailTemplate(
+            Notification notification, string operation, string templatePath, 
+            List<int> toUserIds, string fromName, string ccName, string subject, string html)
+        {
+            var template = new EmailTemplate {
+                TemplatePath = templatePath,
+                ToUserIds = toUserIds.ToArray(),
+                FromEmail = AppSettings.GetString("NotificationsFromEmail"),
+                FromName = fromName,
+                CcEmail = AppSettings.GetString("NotificationsCcEmail"),
+                CcName = ccName,
+                Subject = subject,
+                BodyHtml = html,
+                Created = DateTime.Now,
+            };
+
+            using (var trans = Db.OpenTransaction())
+            {
+                template.Id = await Db.InsertAsync(template, selectIdentity: true);
+
+                var operations = new List<string>(notification.Operations ?? TypeConstants.EmptyStringArray);
+                operations.AddIfNotExists(operation);
+                notification.Started = DateTime.Now;
+                notification.Operations = operations.ToArray();
+                notification.EmailTemplateId = template.Id;
+                notification.UserIds = toUserIds.ToArray();
+                notification.EmailedUserIds = new int[0];
+
+                await Db.UpdateAsync(notification);
+
+                trans.Commit();
+            }
+
+            return template;
+        }
+
+        private async Task RecordEmailSentToUser(long notificationId, int userId)
+        {
+            await Db.ExecuteSqlAsync(@"UPDATE notification SET emailed_user_ids = emailed_user_ids || @userId
+                WHERE id = @id", new { userId, id = notificationId });
+        }
+
+        private async Task SendEmailsToRemainingUsers(Notification notification, EmailTemplate template)
+        {
+            var remainingUserIds = notification.UserIds.Where(x => !notification.EmailedUserIds.Contains(x)).ToList();
+            if (remainingUserIds.Count > 0)
+            {
+                var users = await Db.SelectAsync<UserEmailInfo>(Db.From<CustomUserAuth>()
+                    .Where(x => remainingUserIds.Contains(x.Id)));
+
+                var userMap = users.ToDictionary(x => x.Id);
+
+                foreach (var userId in remainingUserIds)
+                {
+                    var user = userMap[userId];
+                    if (!string.IsNullOrEmpty(user.Email))
+                    {
+                        Email.Send(template.ToEmailMessage(user.Email, user.DisplayName ?? user.UserName));
+                    }
+
+                    await RecordEmailSentToUser(notification.Id, userId);
+                }
+            }
+            else
+            {
+                SendNotificationEmail(template, $"{notification.UserIds.Length} subscribers");
+            }
+        }
+
+        private void SendNotificationEmail(EmailTemplate template, string toName)
+        {
+            var notificationsEmail = AppSettings.GetString("NotificationsFromEmail");
+            var email = template.ToEmailMessage(notificationsEmail, toName);
+            Email.Send(email);
+        }
+    }
+
+    public static class NotificationUtils
+    {
+        public static EmailMessage ToEmailMessage(this EmailTemplate from, string toEmail, string toName)
+        {
+            return new EmailMessage {
+                To = new MailTo {
+                    Email = toEmail,
+                    Name = toName,
+                },
+                From = new MailTo {
+                    Email = from.FromEmail,
+                    Name = from.FromName,
+                },
+                Cc = from.CcEmail == null ? null : new MailTo {
+                    Email = from.CcEmail,
+                    Name = from.CcName,
+                },
+                Subject = from.Subject,
+                Body = from.Body,
+                BodyHtml = from.BodyHtml
+            };
+        }
+    }
+}
---
+++ src/TechStacks.ServiceInterface/Admin/NotificationServices.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Threading.Tasks;
+using ServiceStack;
+using ServiceStack.Configuration;
+using ServiceStack.DataAnnotations;
+using ServiceStack.Logging;
+using ServiceStack.OrmLite;
+using ServiceStack.Templates;
+using TechStacks.ServiceInterface.DataModel;
+using TechStacks.ServiceInterface.Notifications;
+using TechStacks.ServiceModel;
+using TechStacks.ServiceModel.Types;
+
+namespace TechStacks.ServiceInterface.Admin
+{
+    [ExcludeMetadata]
+    [Route("/notifications/{Id}/send")]
+    public class SendNotification : IReturnVoid
+    {
+        public long Id { get; set; }
+    }
+
+    [ExcludeMetadata]
+    [Route("/email/system")]
+    public class SendSystemEmail : IReturnVoid
+    {
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+
+    [RequiredRole("Admin")]
+    public partial class NotificationServices : Service
+    {
+        private static ILog log = LogManager.GetLogger(typeof(NotificationServices));
+        
+        public EmailProvider Email { get; set; } 
+        
+        public IAppSettings AppSettings { get; set; }
+
+        Func<Notification, Task> GetEventHandler(string eventName)
+        {
+            switch (eventName)
+            {
+                case nameof(CreatePost):
+                    return SendNewPostEmail;
+                case nameof(UserPostReport):
+                    return SendReportPostEmail;
+                case nameof(UserPostCommentReport):
+                    return SendReportCommentEmail;
+            }
+            return null;
+        }
+
+        public void Any(SendSystemEmail request)
+        {
+            Email.Send(new EmailMessage {
+                To = new MailTo {
+                    Email = AppSettings.GetString("SystemToEmail"),
+                },
+                From = new MailTo {
+                    Email = AppSettings.GetString("NotificationsFromEmail"),
+                },
+                Subject = $"[SYSTEM] {request.Subject}",
+                Body = request.Body,
+            });
+        }
+        
+        public async Task Any(SendNotification request)
+        {
+            var notification = AssertNotification(request.Id);
+
+            var eventHandler = GetEventHandler(notification.Event);
+            if (eventHandler != null)
+            {
+                try
+                {
+                    await eventHandler(notification);
+
+                    await Db.UpdateOnlyAsync(() => new Notification {
+                            Completed = DateTime.Now
+                        },
+                        where: x => x.Id == notification.Id);
+                }
+                catch (Exception ex)
+                {
+                    await Db.UpdateOnlyAsync(() => new Notification {
+                            Failed = DateTime.Now,
+                            Error = ex.Message + Environment.NewLine + ex
+                        },
+                        where:x => x.Id == notification.Id);
+                    throw;
+                }
+            }
+            else
+            {
+                log.Warn($"Received notification of unknown Event Type: {notification.Event}");
+            }
+        }
+
+        private async Task SendNewPostEmail(Notification notification)
+        {
+            EmailTemplate template = null;
+
+            if (notification.EmailTemplateId == null)
+            {
+                var post = await AssertPost(notification.RefId);
+                var org = await Db.SingleByIdAsync<Organization>(post.OrganizationId);
+                var user = await Db.SingleByIdAsync<CustomUserAuth>(post.UserId);
+
+                var q = Db.From<OrganizationSubscription>()
+                    .Where(x => x.OrganizationId == post.OrganizationId)
+                    .And("ARRAY[{0}] && post_types", post.Type)
+                    .Select(x => x.UserId);
+                var postTypeSubscriberUserIds = await Db.ColumnAsync<int>(q);
+
+                var context = CreateEmailTemplateContext();
+                var templatePath = "emails/post-new";
+                var page = context.GetPage(templatePath);
+                var result = new PageResult(page) {
+                    Args = {
+                        ["baseUrl"] = AppSettings.GetString("PublicBaseUrl"),
+                        ["post"] = post,
+                        ["organization"] = org,
+                    }
+                };
+
+                template = await CreateAndSaveEmailTemplate(notification, nameof(SendNewPostEmail), templatePath, 
+                    toUserIds: postTypeSubscriberUserIds, 
+                    fromName:  user.DisplayName ?? user.UserName, 
+                    ccName:    org.Name + " Subscribed", 
+                    subject:   $"[{post.Type}] {post.Title}", 
+                    html:      await result.RenderToStringAsync());
+            }
+            else
+            {
+                template = await Db.SingleByIdAsync<EmailTemplate>(notification.EmailTemplateId);
+            }
+
+            await SendEmailsToRemainingUsers(notification, template);
+        }
+
+        async Task SendReportPostEmail(Notification notification)
+        {
+            EmailTemplate template = null;
+
+            if (notification.EmailTemplateId == null)
+            {
+                var report = await Db.SingleByIdAsync<PostReport>(notification.RefId);
+                var post = await AssertPost(report.PostId);
+                var org = await Db.SingleByIdAsync<Organization>(post.OrganizationId);
+                var moderatorUserIds = await GetOrganizationModeratorIds(org.Id);
+                
+                var context = CreateEmailTemplateContext();
+                var templatePath = "emails/post-report";
+                var page = context.GetPage(templatePath);
+                var result = new PageResult(page) {
+                    Args = {
+                        ["baseUrl"] = AppSettings.GetString("PublicBaseUrl"),
+                        ["report"] = report,
+                        ["post"] = post,
+                        ["organization"] = org,
+                    }
+                };
+
+                var reportType = report.FlagType == FlagType.Other ? "Report" : report.FlagType.ToString();
+
+                template = await CreateAndSaveEmailTemplate(notification, nameof(SendReportPostEmail), templatePath, 
+                    toUserIds: moderatorUserIds, 
+                    fromName:  report.UserName, 
+                    ccName:    org.Name + " Moderators", 
+                    subject:   $"[{reportType}] {post.Title}", 
+                    html:      await result.RenderToStringAsync());
+            }            
+            else
+            {
+                template = await Db.SingleByIdAsync<EmailTemplate>(notification.EmailTemplateId);
+            }
+
+            await SendEmailsToRemainingUsers(notification, template);
+        }
+
+        async Task SendReportCommentEmail(Notification notification)
+        {
+            EmailTemplate template = null;
+
+            if (notification.EmailTemplateId == null)
+            {
+                var report = await Db.SingleByIdAsync<PostCommentReport>(notification.RefId);
+                var comment = await Db.SingleByIdAsync<PostComment>(report.PostCommentId);                
+                var org = await Db.SingleByIdAsync<Organization>(report.OrganizationId);
+                var moderatorUserIds = await GetOrganizationModeratorIds(org.Id);
+                
+                var context = CreateEmailTemplateContext();
+                var templatePath = "emails/comment-report";
+                var page = context.GetPage(templatePath);
+                var result = new PageResult(page) {
+                    Args = {
+                        ["baseUrl"] = AppSettings.GetString("PublicBaseUrl"),
+                        ["report"] = report,
+                        ["comment"] = comment,
+                        ["organization"] = org,
+                    }
+                };
+
+                var reportType = report.FlagType == FlagType.Other ? "Report" : report.FlagType.ToString();
+
+                template = await CreateAndSaveEmailTemplate(notification, nameof(SendReportPostEmail), templatePath, 
+                    toUserIds: moderatorUserIds, 
+                    fromName:  report.UserName, 
+                    ccName:    org.Name + " Moderators", 
+                    subject:   $"[{reportType}] Comment", 
+                    html:      await result.RenderToStringAsync());
+            }            
+            else
+            {
+                template = await Db.SingleByIdAsync<EmailTemplate>(notification.EmailTemplateId);
+            }
+
+            await SendEmailsToRemainingUsers(notification, template);
+        }
+    }
+
+}
