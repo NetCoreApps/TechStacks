703c51489784da26dfd179f2e6cea9f4e3a28802
Demis Bellot <demis.bellot@gmail.com>
2018-04-20 04:08:53 -0400

    update deploy.sh

--- src/TechStacks/deploy.sh
+++ src/TechStacks/deploy.sh
@@ -1,7 +1,4 @@
 # pre-requisite: npm run publish
 
 rsync -avz -e 'ssh' bin/Release/netcoreapp2.0/publish/ ubuntu@lightsail.web-app.io:/home/deploy/apps/techstacks
-
-#rsync -avz --exclude node_modules/ -e 'ssh' scripts/ ubuntu@lightsail.web-app.io:/home/deploy/apps/scripts
-
 ssh ubuntu@lightsail.web-app.io "sudo supervisorctl restart web-techstacks"
