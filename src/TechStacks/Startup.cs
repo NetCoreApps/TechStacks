3d8b88c9f85f429293ca081a0b3b2e6d2a909815
Demis Bellot <demis.bellot@gmail.com>
2019-09-24 03:11:23 -0400

    Upgrade to .NET Core 3.0

--- src/TechStacks/Startup.cs
+++ src/TechStacks/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Funq;
 using ServiceStack;
 using ServiceStack.Admin;
@@ -37,7 +38,7 @@ namespace TechStacks
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
--- src/TechStacks/deploy.sh
+++ src/TechStacks/deploy.sh
@@ -1,2 +1,2 @@
-rsync -avz -e 'ssh' bin/Release/netcoreapp2.2/publish/ ubuntu@forums.servicestack.net:/home/ubuntu/apps/techstacks
+rsync -avz -e 'ssh' bin/Release/netcoreapp3.0/publish/ ubuntu@forums.servicestack.net:/home/ubuntu/apps/techstacks
 ssh ubuntu@forums.servicestack.net "sudo supervisorctl restart app-techstacks"
