8106681adc7bcc16bb550aeb4aefb5bb7c04a56c
Demis Bellot <demis.bellot@gmail.com>
2019-09-25 17:07:39 -0400

    fix indenting

--- src/TechStacks/Startup.cs
+++ src/TechStacks/Startup.cs
@@ -33,9 +33,7 @@ namespace TechStacks
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+        public void ConfigureServices(IServiceCollection services) { }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -45,8 +43,7 @@ namespace TechStacks
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseServiceStack(new AppHost
-            {
+            app.UseServiceStack(new AppHost {
                 AppSettings = new NetCoreAppSettings(Configuration)
             });
         }
@@ -55,7 +52,7 @@ namespace TechStacks
     public class AppHost : AppHostBase
     {
         public AppHost()
-            : base("TechStacks!", typeof(TechnologyServices).Assembly) {}
+            : base("TechStacks!", typeof(TechnologyServices).Assembly) { }
 
         private static ILog log;
 
@@ -68,17 +65,17 @@ namespace TechStacks
             GetPlugin<NativeTypesFeature>().MetadataTypesConfig.BaseUrl = "https://www.techstacks.io";
 
             var debugMode = AppSettings.Get(nameof(HostConfig.DebugMode), false);
-            SetConfig(new HostConfig
-            {
+            SetConfig(new HostConfig {
                 AddRedirectParamsToQueryString = true,
                 DebugMode = debugMode,
             });
 
-            JsConfig.Init(new ServiceStack.Text.Config { DateHandler = DateHandler.ISO8601 });
+            JsConfig.Init(new ServiceStack.Text.Config {DateHandler = DateHandler.ISO8601});
 
             var dbFactory = new OrmLiteConnectionFactory(
-              Environment.GetEnvironmentVariable("TECHSTACKS_DB") ?? AppSettings.GetString("OrmLite.ConnectionString"),
-              PostgreSqlDialect.Provider);
+                Environment.GetEnvironmentVariable("TECHSTACKS_DB") ??
+                AppSettings.GetString("OrmLite.ConnectionString"),
+                PostgreSqlDialect.Provider);
             dbFactory.RegisterDialectProvider(nameof(PostgreSqlDialect), PostgreSqlDialect.Provider);
 
             container.Register<IDbConnectionFactory>(dbFactory);
@@ -86,25 +83,20 @@ namespace TechStacks
             // enable server-side rendering, see: https://sharpscript.net
             Plugins.Add(new SharpPagesFeature());
 
-            Plugins.Add(new AuthFeature(() => new CustomUserSession(), new IAuthProvider[]
-            {
+            Plugins.Add(new AuthFeature(() => new CustomUserSession(), new IAuthProvider[] {
                 new TwitterAuthProvider(AppSettings),
                 new GithubAuthProvider(AppSettings),
-                new JwtAuthProvider(AppSettings)
-                {
+                new JwtAuthProvider(AppSettings) {
                     RequireSecureConnection = false,
                     IncludeJwtInConvertSessionToTokenResponse = true,
-                    CreatePayloadFilter = (payload, session) =>
-                    {
+                    CreatePayloadFilter = (payload, session) => {
                         var githubAuth = session.ProviderOAuthAccess.Safe()
                             .FirstOrDefault(x => x.Provider == "github");
                         payload["ats"] = githubAuth?.AccessTokenSecret;
                     },
-                    PopulateSessionFilter = (session, obj, req) =>
-                    {
-                        session.ProviderOAuthAccess = new List<IAuthTokens>
-                        {
-                            new AuthTokens { Provider = "github", AccessTokenSecret = obj["ats"] }
+                    PopulateSessionFilter = (session, obj, req) => {
+                        session.ProviderOAuthAccess = new List<IAuthTokens> {
+                            new AuthTokens {Provider = "github", AccessTokenSecret = obj["ats"]}
                         };
                     }
                 },
@@ -112,7 +104,7 @@ namespace TechStacks
                     Provider = "servicestack",
                     DiscourseUrl = "https://forums.servicestack.net",
                 },
-            }){
+            }) {
                 HtmlRedirect = "/"
             });
 
@@ -124,16 +116,18 @@ namespace TechStacks
                 AppSettings.GetString("WebStacks.ConsumerSecret"),
                 AppSettings.GetString("WebStacks.AccessToken"),
                 AppSettings.GetString("WebStacks.AccessSecret")) {
-                    BaseUrl = AppSettings.GetString("PublicBaseUrl"),
-                });
+                BaseUrl = AppSettings.GetString("PublicBaseUrl"),
+            });
 
             container.Register(new EmailProvider {
-              UserName = Environment.GetEnvironmentVariable("TECHSTACKS_SMTP_USER") ?? AppSettings.GetString("smtp.UserName"),
-              Password = Environment.GetEnvironmentVariable("TECHSTACKS_SMTP_PASS") ?? AppSettings.GetString("smtp.Password"),
-              EnableSsl = true,
-              Host = AppSettings.GetString("smtp.Host"),
-              Port = AppSettings.Get<int>("smtp.Port"),
-              Bcc = AppSettings.GetString("smtp.Bcc"),
+                UserName = Environment.GetEnvironmentVariable("TECHSTACKS_SMTP_USER") ??
+                           AppSettings.GetString("smtp.UserName"),
+                Password = Environment.GetEnvironmentVariable("TECHSTACKS_SMTP_PASS") ??
+                           AppSettings.GetString("smtp.Password"),
+                EnableSsl = true,
+                Host = AppSettings.GetString("smtp.Host"),
+                Port = AppSettings.Get<int>("smtp.Port"),
+                Bcc = AppSettings.GetString("smtp.Bcc"),
             });
 
             var authRepo = new OrmLiteAuthRepository<CustomUserAuth, UserAuthDetails>(dbFactory);
@@ -150,17 +144,15 @@ namespace TechStacks
 
                 var baseUrl = "https://techstacks.io";
 
-                Plugins.Add(new SitemapFeature
-                {
+                Plugins.Add(new SitemapFeature {
                     SitemapIndex = {
                         new Sitemap {
                             Location = baseUrl + "/sitemap-techstacks.xml",
                             AtPath = "/sitemap-techstacks.xml",
                             LastModified = DateTime.UtcNow,
                             UrlSet = db.Select(db.From<TechnologyStack>().OrderByDescending(x => x.LastModified))
-                                .Map(x => new SitemapUrl
-                                {
-                                    Location = baseUrl + new ClientTechnologyStack { Slug = x.Slug }.ToAbsoluteUri(),
+                                .Map(x => new SitemapUrl {
+                                    Location = baseUrl + new ClientTechnologyStack {Slug = x.Slug}.ToAbsoluteUri(),
                                     LastModified = x.LastModified,
                                     ChangeFrequency = SitemapFrequency.Weekly,
                                 }),
@@ -170,22 +162,19 @@ namespace TechStacks
                             AtPath = "/sitemap-technologies.xml",
                             LastModified = DateTime.UtcNow,
                             UrlSet = db.Select(db.From<Technology>().OrderByDescending(x => x.LastModified))
-                                .Map(x => new SitemapUrl
-                                {
-                                    Location = baseUrl + new ClientTechnology { Slug = x.Slug }.ToAbsoluteUri(),
+                                .Map(x => new SitemapUrl {
+                                    Location = baseUrl + new ClientTechnology {Slug = x.Slug}.ToAbsoluteUri(),
                                     LastModified = x.LastModified,
                                     ChangeFrequency = SitemapFrequency.Weekly,
                                 })
                         },
-                        new Sitemap
-                        {
+                        new Sitemap {
                             Location = baseUrl + "/sitemap-users.xml",
                             AtPath = "/sitemap-users.xml",
                             LastModified = DateTime.UtcNow,
                             UrlSet = db.Select(db.From<CustomUserAuth>().OrderByDescending(x => x.ModifiedDate))
-                                .Map(x => new SitemapUrl
-                                {
-                                    Location = baseUrl + new ClientUser { UserName = x.UserName }.ToAbsoluteUri(),
+                                .Map(x => new SitemapUrl {
+                                    Location = baseUrl + new ClientUser {UserName = x.UserName}.ToAbsoluteUri(),
                                     LastModified = x.ModifiedDate,
                                     ChangeFrequency = SitemapFrequency.Weekly,
                                 })
@@ -194,9 +183,9 @@ namespace TechStacks
                             Location = baseUrl + "/sitemap-organizations.xml",
                             AtPath = "/sitemap-organizations.xml",
                             LastModified = DateTime.UtcNow,
-                            UrlSet = db.Select(db.From<Organization>().Where(x => x.Deleted == null).OrderByDescending(x => x.Modified))
-                                .Map(x => new SitemapUrl
-                                {
+                            UrlSet = db.Select(db.From<Organization>().Where(x => x.Deleted == null)
+                                    .OrderByDescending(x => x.Modified))
+                                .Map(x => new SitemapUrl {
                                     Location = baseUrl + $"/{x.Slug}",
                                     LastModified = x.Modified,
                                     ChangeFrequency = SitemapFrequency.Weekly,
@@ -206,9 +195,10 @@ namespace TechStacks
                             Location = baseUrl + "/sitemap-posts.xml",
                             AtPath = "/sitemap-posts.xml",
                             LastModified = DateTime.UtcNow,
-                            UrlSet = db.Select(db.From<Post>().Where(x => x.Type != PostType.Question && x.Deleted == null && x.Hidden == null).Take(1000).OrderByDescending(x => x.Modified))
-                                .Map(x => new SitemapUrl
-                                {
+                            UrlSet = db.Select(db.From<Post>()
+                                    .Where(x => x.Type != PostType.Question && x.Deleted == null && x.Hidden == null)
+                                    .Take(1000).OrderByDescending(x => x.Modified))
+                                .Map(x => new SitemapUrl {
                                     Location = baseUrl + $"/posts/{x.Id}/{x.Slug}",
                                     LastModified = x.Modified,
                                     ChangeFrequency = SitemapFrequency.Hourly,
@@ -219,8 +209,11 @@ namespace TechStacks
             }
 
             Plugins.Add(new CorsFeature(
-                allowOriginWhitelist: new[] { "https://techstacks.io", "https://www.techstacks.io",
-                  "http://localhost:3000", "http://localhost:16325", "http://localhost:8080", "http://null.jsbin.com", "http://run.plnkr.co" },
+                allowOriginWhitelist: new[] {
+                    "https://techstacks.io", "https://www.techstacks.io",
+                    "http://localhost:3000", "http://localhost:16325", "http://localhost:8080", "http://null.jsbin.com",
+                    "http://run.plnkr.co"
+                },
                 allowCredentials: true,
                 allowedHeaders: "Content-Type, Allow, Authorization",
                 maxAge: 60 * 60)); //Cache OPTIONS permissions
@@ -229,10 +222,8 @@ namespace TechStacks
             container.RegisterValidators(typeof(AppHost).Assembly);
             container.RegisterValidators(typeof(TechnologyServices).Assembly);
 
-            Plugins.Add(new AutoQueryMetadataFeature
-            {
-                AutoQueryViewerConfig =
-                {
+            Plugins.Add(new AutoQueryMetadataFeature {
+                AutoQueryViewerConfig = {
                     ServiceDescription = "Discover what technologies were used to create popular Websites and Apps",
                     ServiceIconUrl = "/img/app/logo-76.png",
                     BackgroundColor = "#0095F5",
@@ -245,19 +236,17 @@ namespace TechStacks
                     OnlyShowAnnotatedServices = true,
                 }
             });
-            Plugins.Add(new AutoQueryFeature
-            {
+            Plugins.Add(new AutoQueryFeature {
                 MaxLimit = 500,
                 StripUpperInLike = false,
-                ResponseFilters =
-                {
-                   ctx => ctx.Response.Meta["Cache"] = Stopwatch.GetTimestamp().ToString()
+                ResponseFilters = {
+                    ctx => ctx.Response.Meta["Cache"] = Stopwatch.GetTimestamp().ToString()
                 }
             });
             Plugins.Add(new AdminFeature());
             Plugins.Add(new OpenApiFeature());
 
-            RegisterTypedRequestFilter<IRegisterStats>((req,res,dto) =>
+            RegisterTypedRequestFilter<IRegisterStats>((req, res, dto) =>
                 dbFactory.RegisterPageView(dto.GetStatsId()));
 
             if (Config.DebugMode)
