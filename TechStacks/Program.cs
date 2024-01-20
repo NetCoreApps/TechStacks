1fa5e8d5d8231e3131180cf5bd4fc3e73cfd6440
Demis Bellot <demis.bellot@gmail.com>
2024-01-20 19:46:43 +0800

    always enable Swagger

--- TechStacks/Program.cs
+++ TechStacks/Program.cs
@@ -109,14 +109,12 @@ builder.Services.AddServiceStack(typeof(TechnologyServices).Assembly, c => {
 
 var app = builder.Build();
 
+app.UseMigrationsEndPoint();
+app.UseSwagger();
+app.UseSwaggerUI();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-else
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
